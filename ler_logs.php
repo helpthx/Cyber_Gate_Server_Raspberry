8a9b102f5ed349fe49fde74401230a590d8471e2
João Vitor Rodrigues Baptista <jvrbaptista@live.com>
2018-12-03 01:59:28 -0200

    [IMP] Ultima funcionalidade do web

--- ler_logs.php
+++ ler_logs.php
@@ -1,27 +1,56 @@
 <?php
 
+$controlador;
 
 
+if ($controlador == 1 ) {
 
+	$arquivo = fopen ('/home/pi/Arquivos/Logs/creditos.txt', 'r');
 
+	// Lê o conteúdo do arquivo 
+	while(!feof($arquivo))
+	{
 
+		$linha = fgets($arquivo, 1024);
+			echo $linha.'<br />';
+	}
 
+	fclose($arquivo);
 
-
-
-// Abre o Arquvio no Modo r (para leitura)
-$arquivo = fopen ('Logs/creditos.txt', 'r');
-
-// Lê o conteúdo do arquivo 
-while(!feof($arquivo))
-{
-//Mostra uma linha do arquivo
-$linha = fgets($arquivo, 1024);
-	echo $linha.'<br />';
 }
 
-// Fecha arquivo aberto
-fclose($arquivo);
-?>
+elseif ($controlador == 2) {
+	$arquivo = fopen ('/home/pi/Arquivos/Logs/log_refeicoes.txt', 'r');
 
-excluidos.txt
+	// Lê o conteúdo do arquivo 
+	while(!feof($arquivo))
+	{
+
+		$linha = fgets($arquivo, 1024);
+			echo $linha.'<br />';
+	}
+
+	fclose($arquivo);
+}
+
+elseif ($controlador == 3) {
+	$arquivo = fopen ('/home/pi/Arquivos/Logs/excluidos.txt', 'r');
+
+	// Lê o conteúdo do arquivo 
+	while(!feof($arquivo))
+	{
+
+		$linha = fgets($arquivo, 1024);
+			echo $linha.'<br />';
+	}
+
+	fclose($arquivo);
+}
+
+else{
+
+	echo 'Ops algum erro aconteceu';
+}
+
+
+?>
